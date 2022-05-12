9b3808f8ffcd1772ddff29b753d2625b1e75ca5e
a131mael <a131mael@gmail.com>
2022-05-12 18:07:36 -0300

    estavel

--- src/org/aaf/dto/nfs/Contants.java
+++ src/org/aaf/dto/nfs/Contants.java
@@ -2,8 +2,9 @@ package org.aaf.dto.nfs;
 
 public class Contants {
 
-	public static String URL = "http://sync.nfs-e.net/datacenter/include/nfw/importa_nfw/nfw_import_upload.php";
+	//public static String URL = "http://sync.nfs-e.net/datacenter/include/nfw/importa_nfw/nfw_import_upload.php";
+	public static String URL = "https://palhoca.atende.net:7443/atende.php?pg=rest&service=WNERestServiceNFSe&cidade=padrao";
 	public static String  login = "14395954000155";
-	public static String senha= "stratus01"; 
+	public static String senha= "Stratus#01"; 
 	public static String cidade= "8233"; 
 }
